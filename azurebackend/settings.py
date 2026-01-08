021bb90401f6c56da3c7e447bcc3c030ff69e7fa
Omar Frikha <omar.frikha96@gmail.com>
2026-01-08 17:11:20 +0100

    add setting

--- azurebackend/settings.py
+++ azurebackend/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True # For development, allows frontend to access API
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'inventory'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
