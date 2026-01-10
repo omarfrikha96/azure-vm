8d039bad0acbfa62a81afc22ff7090a78c1eef03
Omar Frikha <omar.frikha96@gmail.com>
2026-01-10 11:04:33 +0100

    add doven

--- azurebackend/settings.py
+++ azurebackend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+# Load environment variables from .env file
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
